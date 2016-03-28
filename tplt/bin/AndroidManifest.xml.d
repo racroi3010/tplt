e07dc46ba85df5671555689b3cc9c3a0d953c3c7
my pet <my pet@mypet-PC>
2016-03-28 17:27:08 +0900

    first commit

--- tplt/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-E:\workspace_android\git_home\tplt\tplt\bin\AndroidManifest.xml : \
-E:\workspace_android\git_home\tplt\tplt\AndroidManifest.xml \
--- tplt/bin/classes.dex.d
+++
@@ -1,2 +0,0 @@
-E:\workspace_android\git_home\tplt\tplt\bin\classes.dex : \
-E:\workspace_android\git_home\tplt\tplt\bin\proguard\obfuscated.jar \
